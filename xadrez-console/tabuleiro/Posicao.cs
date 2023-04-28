f3c7e897f30b85d75aa0f781943e067d9c37e223
Eliseu Medeiros <eliseujayro@gmail.com>
2023-04-27 23:03:28 -0300

    Criando a classe Posicao

---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,18 @@
+﻿namespace tabuleiro {
+    internal class Posicao {
+
+        public int line { get; set; }
+        public int column { get; set; }
+
+        public Posicao(int line, int column) {
+            this.line = line;
+            this.column = column;
+        }
+
+        public override string ToString() {
+            return line
+                + ", " 
+                + column;
+        }
+    }
+}
